57e8d6e29e3369ab398693cdc4a64f02379660a0
Donato-Carola <don94@live.it>
2023-11-10 17:39:27 +0100

    fine esercizio con correzioni

--- js/script.js
+++ js/script.js
@@ -17,39 +17,39 @@ console.log("eta passeggero: " + etàPasseggero);
 
                                    //2) moltiplicare i kilometri dichirati dal passeggero per 0.21euro al km
 
-const priceForKm = (kmDaSvolgere * 0.21).toFixed(2);
+const priceForKm = (kmDaSvolgere * 0.21);
 console.log( 'prezzo moltiplicato per 0.21 euro ' + priceForKm);
 
                                    //3)if il passeggero è minorenne fai lo sconto del 20% al prezzo totale calcolato con la moltiplicazione altrimenti prezzo senza sconto
 
 if (etàPasseggero < 18) {
-  const PriceTotale = ((priceForKm * 20) / 100).toFixed(2);
-  const Sconto = (priceForKm - PriceTotale).toFixed(2)
+  const  sconto= ((priceForKm * 20) / 100);
+  const priceTotale = (priceForKm - sconto).toFixed(2)
   
 
   //lascio console.log nel codice per vedere nell'inspector cosa succede
-  console.log( 'totale prezzo da sottrarre come sconto' + PriceTotale)
+  console.log( 'totale prezzo da sottrarre come sconto' + sconto)
 
   //per visualizzarlo a schermo
   document.getElementById("Percentuale").innerHTML = "il prezzo totale, con il 20% di sconto è: ";
     
-  document.getElementById("Price").innerHTML = Sconto + " Euro";
+  document.getElementById("Price").innerHTML = priceTotale + " Euro";
 
 
                                     //4)if il passeggeo ha più di 65 anni fai losconto del 40% al prezzo calcolato con la moltiplicazione altrimeti prezzo senza sconto
 
 } else if (etàPasseggero > 65) {
-  const PriceTotale = ((priceForKm * 40) / 100).toFixed(2);
-  const Sconto = (priceForKm - PriceTotale).toFixed(2);
-  
+  const  sconto= ((priceForKm * 40) / 100);
+  const priceTotale = (priceForKm - sconto).toFixed(2);
+
   //lascio console.log nel codice per vedere nell'inspector cosa succede
-  console.log( 'totale prezzo da sottrarre come sconto' + PriceTotale)
+  console.log( 'totale prezzo da sottrarre come sconto' + sconto)
  
 
   //per visualizzarlo a schermo
   document.getElementById("Percentuale").innerHTML = "il prezzo totale, con il 40% di sconto è: ";
     
-  document.getElementById("Price").innerHTML = Sconto + " Euro";
+  document.getElementById("Price").innerHTML = priceTotale + " Euro";
 
 
 
