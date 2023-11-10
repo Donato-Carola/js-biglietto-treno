55bcd26411f6aafbe453be0b54e107f8c58f0339
Donato-Carola <don94@live.it>
2023-11-10 15:57:08 +0100

    primo macropensiero su come svolgere l'esercizio odierno

--- js/script.js
+++ js/script.js
@@ -7,3 +7,24 @@ va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va scritto in pagina in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).*/
 
+//Macropensiero per svolgere l'esercizio 
+
+
+
+//1) Chiedere all'utente  il numero di kilometri che vuole percorrere e l'età del passeggero
+
+    
+
+
+
+//2) moltiplicare i kilometri dichirati dal passeggero per 0.21euro al km
+
+//3)if il passeggero è minorenne fai lo sconto del 20% al prezzo totale calcolato con la moltiplicazione altrimenti prezzo senza sconto
+
+//4)if il passeggeo ha più di 65 anni fai losconto del 40% al prezzo calcolato con la moltiplicazione altrimeti prezzo senza sconto
+
+//5)scrivi il prezzo totale sullo schermo e non con console.log()
+
+
+
+
